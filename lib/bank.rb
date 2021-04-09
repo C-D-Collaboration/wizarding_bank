8e9dd9fbd3ff1d0ad33ac92c5c435882b8fa8e2e
Dani Coleman <danicoleman00@gmail.com>
2021-04-09 10:08:47 -0500

    Add withdrawal method
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,6 +2,7 @@ class Bank
   attr_reader :name
   def initialize(name)
     @name = name
+    @account_balance = 0
   end
 
   def open_account(person)
@@ -9,11 +10,21 @@ class Bank
   end
 
   def deposit(person, amount)
+    @account_balance = amount
     remainder = person.cash - amount
-    if person.cash < amount 
+    if person.cash < amount
       "#{person.name} does not have enough cash to perform this deposit."
     else
       "#{amount} galleons have been deposited into #{person.name}'s #{@name} account. Balance: #{amount} Cash: #{remainder}"
+    end
+  end
+
+  def withdrawal(person, withdrawal_amount)
+    remainder = @account_balance - withdrawal_amount
+    if @account_balance < withdrawal_amount
+      "Insufficient funds."
+    else
+      "#{person.name} has withdrawn #{withdrawal_amount} galleons. Balance: #{remainder}"
     end
   end
 end
