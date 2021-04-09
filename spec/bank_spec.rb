600025f41d733861eb619a36b70a2f7acad5b91f
Dani Coleman <danicoleman00@gmail.com>
2021-04-09 10:09:11 -0500

    Add happy and sad path test for withdrawal method
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,7 +1,8 @@
 require 'rspec'
+require './lib/person'
 require './lib/bank'
 
-RSpec.describe Bank do 
+RSpec.describe Bank do
   it 'can create an instance of a Bank class' do
     dcu = Bank.new("Dani's Credit Union")
 
@@ -23,10 +24,26 @@ RSpec.describe Bank do
     expect(dcu.deposit(dani, 750)).to eq("750 galleons have been deposited into #{dani.name}'s #{dcu.name} account. Balance: 750 Cash: 250")
   end
 
-  it 'can not deposit more cash than they have' do 
+  it 'can not deposit more cash than they have' do
     dcu = Bank.new("Dani's Credit Union")
     dani = Person.new("Dani", 1000)
     dcu.deposit(dani, 5000)
     expect(dcu.deposit(dani, 5000)).to eq("#{dani.name} does not have enough cash to perform this deposit.")
+  end
+
+  it "can have withdrawals with a person and amount" do
+    dcu = Bank.new("Dani's Credit Union")
+    dani = Person.new("Dani", 1000)
+    dcu.deposit(dani, 750)
+
+    expect(dcu.withdrawal(dani, 250)).to eq("#{dani.name} has withdrawn 250 galleons. Balance: 500")
+  end
+
+  it 'can not withdrawal more cash than they have' do
+    dcu = Bank.new("Dani's Credit Union")
+    dani = Person.new("Dani", 1000)
+    dcu.deposit(dani, 750)
+
+    expect(dcu.withdrawal(dani, 25000)).to eq("Insufficient funds.")
   end
 end
