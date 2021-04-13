74981e7a56fbf96cf97395dbaa58e3941f63d9c0
Dani Coleman <danicoleman00@gmail.com>
2021-04-13 09:38:58 -0500

    Add test to get total cash balance
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -52,17 +52,20 @@ RSpec.describe Bank do
     ccu = Bank.new("Chris's Credit Union")
     dani = Person.new("Dani", 1000)
     dcu.deposit(dani, 750)
-    dcu.add_account(dcu)
+    dcu.add_account(dcu, dani)
 
     expect(dcu.transfer(dani, ccu, 250)).to eq("#{dani.name} has transferred 250 galleons from #{dcu.name} to #{ccu.name}.")
   end
 
   it "can't transfer more money than their balance" do
     dani = Person.new("Dani", 1000)
+    chris = Person.new("Chris", 1000)
     dcu = Bank.new("Dani's Credit Union")
     ccu = Bank.new("Chris's Credit Union")
     dcu.deposit(dani, 750)
-    dcu.add_account(dcu)
+    ccu.deposit(chris, 250)
+    dcu.add_account(dcu, dani)
+    ccu.add_account(ccu, chris)
 
     expect(dcu.transfer(dani, ccu, 25000)).to eq("Insufficient funds.")
   end
@@ -74,5 +77,14 @@ RSpec.describe Bank do
     dcu.deposit(dani, 750)
     # dcu.add_account(dcu)
     expect(dcu.transfer(dani, ccu, 250)).to eq("#{dani.name} does not have an account with #{ccu.name}")
+  end
+
+  it "can get current total cash in the bank" do
+    dani = Person.new("Dani", 1000)
+    dcu = Bank.new("Dani's Credit Union")
+    ccu = Bank.new("Chris's Credit Union")
+    dcu.deposit(dani, 750)
+
+    expect(dcu.total_cash).to eq("Total Cash: 750 galleons")
   end
 end
